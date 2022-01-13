d61d3c72e8d64902bd33ba096a6d4c63fe8c83dc
1024Programming <team1024programming@gmail.com>
2022-01-13 16:48:17 -0500

    Adding intake subsystem, testing multiple merges

---
+++ src/main/java/frc/robot/subsystems/Intake.java
@@ -0,0 +1,27 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.subsystems;
+
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+public class Intake extends SubsystemBase {
+  /** Creates a new Intake. */
+  public Intake() {}
+/**spin intake forward and reverse*/
+  public void intakeForward() {
+
+  }
+
+  public void intakeBack() {
+    
+  }
+  
+  @Override
+  public void periodic() {
+    // This method will be called once per scheduler run
+  }
+
+
+}
